e74f73562b5ab7f0574ace520506ad09a04cd237
Neil Saunders <neilfws@gmail.com>
2026-02-18 17:18:27 +1100

    initial commit

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,16 @@
+var store = [,{
+    "title": "All about Secondary Succession",
+    "excerpt":"Broadly-speaking, this site is a place to record my thoughts and feelings as I interact with the natural world. Why is it named “Secondary Succession” ? The term is used in ecology to explain: the process of ecological community recovery that occurs after a disturbance destroys an existing ecosystem but...","url": "https://neilfws.github.io/SecondarySuccession/about/"
+  },{
+    "title": "Content by category",
+    "excerpt":" ","url": "https://neilfws.github.io/SecondarySuccession/categories/"
+  },{
+    "title": "Welcome",
+    "excerpt":"No posts yet! Check back very soon.     ","url": "https://neilfws.github.io/SecondarySuccession/"
+  },{
+    "title": "Content by year",
+    "excerpt":" ","url": "https://neilfws.github.io/SecondarySuccession/posts/"
+  },{
+    "title": "Content by tag",
+    "excerpt":" ","url": "https://neilfws.github.io/SecondarySuccession/tags/"
+  }]
