5412389c173988bec9741b925a95bb6fbd122bdb
Neil Saunders <neilfws@gmail.com>
2026-04-07 07:29:16 +1000

    Added a test post to check CSS issue

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
-var store = [,{
+var store = [{
+        "title": "On weeds",
+        "excerpt":"My thoughts on weeds. A test post.  ","categories": ["australian native gardening"],
+        "tags": ["weeds","bushcare"],
+        "url": "/posts/2026-04-06-on-weeds/",
+        "teaser": null
+      },{
     "title": "All about Secondary Succession",
     "excerpt":"Broadly-speaking, this site is a place to record my thoughts and feelings as I interact with the natural world. Why is it named “Secondary Succession” ? The term is used in ecology to explain: the process of ecological community recovery that occurs after a disturbance destroys an existing ecosystem but...","url": "https://neilfws.github.io/about/"
   },{
@@ -13,4 +19,13 @@ var store = [,{
   },{
     "title": "Content by tag",
     "excerpt":" ","url": "https://neilfws.github.io/tags/"
+  },{
+    "title": "weeds",
+    "excerpt":"","url": "https://neilfws.github.io/tags/weeds/"
+  },{
+    "title": "bushcare",
+    "excerpt":"","url": "https://neilfws.github.io/tags/bushcare/"
+  },{
+    "title": "australian native gardening",
+    "excerpt":"","url": "https://neilfws.github.io/categories/australian-native-gardening/"
   }]
